6b94e5dc8ee2859e3d38b24362c7172f83bb2de2
Blair Wang <blairwang@outlook.com>
2015-03-09 01:21:37 +1100

    minor bug fixes

--- ui-BwAnalysis.php
+++ ui-BwAnalysis.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Literature Coding</title>
+		<title>BW Analysis Progress</title>
 		<meta charset="utf-8">
 		<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
